42d548c2918a465975491b3d2dd154f776ba2455
Kris Pinter <kris.pinter@brighthr.com>
2018-07-15 11:04:03 +0200

    Revert "add react view engine"
    This reverts commit 8e0e2cd323a14beddc5a9ce42c251d72ce5955db.

--- components/dashboard.jsx
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-
-class HelloMessage extends React.Component {
-    render() {
-        return <div>Hello {this.props.name}</div>;
-    }
-}
-
-module.exports = HelloMessage;
